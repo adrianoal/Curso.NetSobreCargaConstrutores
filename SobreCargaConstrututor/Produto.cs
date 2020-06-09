172eacb2c376ec93ed965a7447f31b4657d0c563
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-06-09 10:22:34 -0300

    Conceito de sobrecarga e sintaxe alternativa

---
+++ SobreCargaConstrututor/Produto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConstruturesAula51
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //Criando construtores - conceito de sobre carga:
+
+        //Construtor com 3 parametros
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        //Construtor com 2 parametros
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;            
+        }
+
+        //Construtur padrao sem parametros
+        public Produto()
+        {
+
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+
+        // P/ fazer uma sobreposicao, tem q escrever a palavra "override"
+        public override string ToString()
+        {
+            return Nome
+                + ", R$ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: R$ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+
+    }
+}
---
+++ SobreCargaConstrututor/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConstruturesAula51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Entre com os dados do Produto: ");
+            Console.WriteLine(String.Empty);
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Console.Write("Quantidade no Estoque: ");
+            //int quantidade = int.Parse(Console.ReadLine());               
+
+            Produto p2 = new Produto();
+            Produto p = new Produto(nome, preco);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do Produto: " + p);
+            Console.WriteLine(string.Empty);// Quebra de linha
+
+            // ADICIONADO NO ESTOQUE ESTOQUE
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qtde = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qtde);
+
+            Console.WriteLine(); // Quebra de linha 
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+
+            // ADICIONADO NO REMOVIDO DO ESTOQUE
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qtde = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qtde);
+
+            Console.WriteLine(); // Quebra de linha 
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+            */
+
+            // Sintaxe alternativa para inicializar valores
+
+            // Esta sintaxe funciona perfeitamente, mesmo q a classe na tem contrutores!
+
+            Produto p3 = new Produto { Nome = "TV", Preco = 500.00, Quantidade = 20 }; // Essa é uma forma de instanciar o produto
+
+            //Obs: Não pode usar essa sintaxe alternativa se a classe tiver contrutores.
+            // Para que isso seja possivel, tem que criar na classe o contrutor padrao.
+
+        }
+    }
+}
